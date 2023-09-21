ea96af041e84b8822bb9a0f96fc229b6af532f6d
praveenkr1506 <99966938+praveenkr1506@users.noreply.github.com>
2023-09-22 01:23:25 +0530

    Updated

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import cors from "cors";
 
@@ -15,6 +16,10 @@ dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
